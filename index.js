ccbd864bdd82e97208129387f6d0057d7688e4dd
Summer Technology Pty Ltd <tatallah@summertech.com.au>
2025-04-01 18:18:35 +1100

    fix5

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const app = express();
 app.use(express.json());
